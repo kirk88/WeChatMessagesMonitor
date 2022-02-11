71759f6db7a49bca6d9994759be36fd53d856f4b
kirk88 <481314821@qq.com>
2022-02-11 09:43:45 +0800

    Initial Commit

--- app/src/main/java/com/background/wechatmessagesmonitor/data/MessagesUploadManager.kt
+++ app/src/main/java/com/background/wechatmessagesmonitor/data/MessagesUploadManager.kt
@@ -1,8 +1,11 @@
 package com.background.wechatmessagesmonitor.data
 
 import android.util.LruCache
+import android.view.accessibility.AccessibilityNodeInfo
 import com.background.wechatmessagesmonitor.applicationContext
+import com.background.wechatmessagesmonitor.constants.*
 import com.background.wechatmessagesmonitor.data.model.WechatMessage
+import com.background.wechatmessagesmonitor.data.model.createMessage
 import com.background.wechatmessagesmonitor.service.WechatForegroundService
 import com.background.wechatmessagesmonitor.utils.Logger
 import com.background.wechatmessagesmonitor.utils.isServiceWork
@@ -10,6 +13,7 @@ import com.background.wechatmessagesmonitor.utils.startServiceCompat
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 
+@OptIn(DelicateCoroutinesApi::class)
 object MessagesUploadManager {
 
     private val cacheMessages = LruCache<Int, WechatMessage>(6000)
@@ -20,22 +24,18 @@ object MessagesUploadManager {
         Logger.error(throwable) { "Receive Message" }
     }
 
+    fun addMessagesFromNode(nodeInfo: AccessibilityNodeInfo) =
+        GlobalScope.launch(Dispatchers.Default + errorHandler) {
+            val messages = getMessages(nodeInfo)
+
+            addMessages(messages)
+        }
+
     fun addMessage(message: WechatMessage) = addMessages(listOf(message))
 
     fun addMessages(messages: Collection<WechatMessage>) =
-        GlobalScope.launch(Dispatchers.IO + errorHandler) {
-            if (!applicationContext.isServiceWork(WechatForegroundService::class.java)) {
-                applicationContext.startServiceCompat(WechatForegroundService::class.java)
-            }
-
-            for (msg in messages) {
-                val key = msg.hashCode()
-                if (cacheMessages.get(key) == null) {
-                    Logger.debug { msg }
-                    cacheMessages.put(key, msg)
-                    channel.send(msg)
-                }
-            }
+        GlobalScope.launch(Dispatchers.Default + errorHandler) {
+            addMessagesInternal(messages)
         }
 
     fun collectMessages(block: suspend (WechatMessage) -> Unit): Job =
@@ -45,5 +45,168 @@ object MessagesUploadManager {
             }
         }
 
+    private suspend fun addMessagesInternal(messages: Collection<WechatMessage>) {
+        if (!applicationContext.isServiceWork(WechatForegroundService::class.java)) {
+            applicationContext.startServiceCompat(WechatForegroundService::class.java)
+        }
+
+        for (msg in messages) {
+            val key = msg.hashCode()
+            if (cacheMessages.get(key) == null) {
+                Logger.debug { msg }
+                cacheMessages.put(key, msg)
+                channel.send(msg)
+            }
+        }
+    }
+
+    private fun getMessages(node: AccessibilityNodeInfo?): List<WechatMessage> {
+        node ?: return emptyList()
+        val contentNode = node.findContentNode() ?: return emptyList()
+        val isFromGroup = contentNode.isFromGroup()
+
+        val messages = mutableListOf<WechatMessage>()
+
+        var messageIndex = 0
+        var time: String? = null
+
+        contentNode.children().forEach {
+            var name: String? = null
+            var nameIndex = 0
+            var content: String? = null
+            var contentIndex = 0
+
+            var redPacketContent: String? = null
+            var mediaType: String? = null
+
+            it.findTimeNode()?.text?.toString()?.let { text ->
+                time = text
+            }
+
+            for (child in it.children()) {
+                if (child.viewIdResourceName in WECHAT_AVATAR_NODE_VIEW_IDS) {
+                    name = child.contentDescription?.replace("头像".toRegex(), "")
+                }
+
+                for ((i, c) in child.children().withIndex()) {
+                    val id = c.viewIdResourceName
+
+                    if (id in WECHAT_AVATAR_NODE_VIEW_IDS && name == null) {
+                        name = c.contentDescription?.replace("头像".toRegex(), "")
+                        nameIndex = i
+                    }
+
+                    if (id in WECHAT_REDPACKET_CONTENT_NODE_VIEW_IDS) {
+                        redPacketContent = c.text?.toString()
+                    }
+
+                    if (id in WECHAT_REDPACKET_NODE_VIEW_IDS && content == null) {
+                        content = "[${c.text?.toString()}]${redPacketContent.orEmpty()}"
+                        contentIndex = i
+                    }
+
+                    if (id in WECHAT_TRANSFER_NODE_VIEW_IDS && content == null) {
+                        content = "[${c.text?.toString()}]"
+                        contentIndex = i
+                    }
+
+                    if (id in WECHAT_SHARE_CONTENT_NODE_VIEW_IDS && content == null) {
+                        content = "[分享]${c.text?.toString()}"
+                        contentIndex = i
+                    }
+
+                    if (id in WECHAT_FILE_CONTENT_NODE_VIEW_IDS && content == null) {
+                        content = "[文件]${c.text?.toString()}"
+                        contentIndex = i
+                    }
+
+                    if (id in WECHAT_VIDEO_CONTENT_NODE_VIEW_IDS && content == null) {
+                        content = "[视频]${c.text?.toString()}"
+                        contentIndex = i
+                    }
+
+                    if (id in WECHAT_IMAGE_CONTENT_NODE_VIEW_IDS && content == null) {
+                        content = "[图片]"
+                        contentIndex = i
+                    }
+
+                    if (id in WECHAT_AUDIO_CONTENT_NODE_VIEW_IDS) {
+                        mediaType = "[语音]"
+                    }
+
+                    if (id in WECHAT_TEXT_CONTENT_NODE_VIEW_IDS && content == null) {
+                        content = "${mediaType.orEmpty()}${c.text?.toString()}"
+                        contentIndex = i
+                    }
+                }
+                if (nameIndex > contentIndex) {
+                    Logger.debug { "自己发送的" }
+                }
+                if (name != null && content != null) {
+                    Logger.debug { "onAccessibilityEvent: $time  $name  $content" }
+                    val message = kotlin.runCatching {
+                        createMessage(
+                            name,
+                            content,
+                            time = time,
+                            from = if (isFromGroup) "群聊" else "个人",
+                            type = 1,
+                            index = messageIndex
+                        )
+                    }.getOrNull()
+
+                    if (message != null) {
+                        messages.add(message)
+                    }
+
+                    messageIndex += 1
+                }
+            }
+        }
+
+        return messages
+    }
+
+
+    private fun AccessibilityNodeInfo.findContentNode(): AccessibilityNodeInfo? {
+        for (viewId in WECHAT_MESSAGE_LIST_VIEW_IDS) {
+            val node = findAccessibilityNodeInfosByViewId(viewId)
+                ?.firstOrNull()
+            if (node != null) {
+                return node
+            }
+        }
+        return null
+    }
+
+    private fun AccessibilityNodeInfo.findTimeNode(): AccessibilityNodeInfo? {
+        for (viewId in WECHAT_TIME_NODE_VIEW_IDS) {
+            val node = findAccessibilityNodeInfosByViewId(viewId)
+                ?.firstOrNull()
+            if (node != null) {
+                return node
+            }
+        }
+        return null
+    }
+
+    private fun AccessibilityNodeInfo.isFromGroup(): Boolean {
+        for (viewId in WECHAT_GROUND_JUDGE_VIEW_IDS) {
+            val nodes = findAccessibilityNodeInfosByViewId(viewId)
+            if (!nodes.isNullOrEmpty()) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun AccessibilityNodeInfo.children(): List<AccessibilityNodeInfo> {
+        val nodes = mutableListOf<AccessibilityNodeInfo>()
+        for (index in 0 until childCount) {
+            val child = getChild(index) ?: continue
+            nodes.add(child)
+        }
+        return nodes
+    }
 
 }
--- app/src/main/java/com/background/wechatmessagesmonitor/service/WechatAccessibilityService.kt
+++ app/src/main/java/com/background/wechatmessagesmonitor/service/WechatAccessibilityService.kt
@@ -4,15 +4,15 @@ import android.accessibilityservice.AccessibilityService
 import android.content.Intent
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
-import com.background.wechatmessagesmonitor.constants.*
+import com.background.wechatmessagesmonitor.constants.KEY_ACCESSIBILITY_SERVICE_HAS_KILLED
+import com.background.wechatmessagesmonitor.constants.NOTIFICATION_ID
 import com.background.wechatmessagesmonitor.data.MessagesUploadManager
-import com.background.wechatmessagesmonitor.data.model.WechatMessage
-import com.background.wechatmessagesmonitor.data.model.createMessage
 import com.background.wechatmessagesmonitor.utils.Logger
 import com.background.wechatmessagesmonitor.utils.Prefs
 import com.background.wechatmessagesmonitor.utils.createNotification
 
 class WechatAccessibilityService : AccessibilityService() {
+
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Logger.debug { "WechatAccessibilityService onStartCommand" }
@@ -20,10 +20,11 @@ class WechatAccessibilityService : AccessibilityService() {
     }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
-        val rootNode = rootInActiveWindow
-        uploadMessages(rootNode)
+        rootInActiveWindow?.let {
+            MessagesUploadManager.addMessagesFromNode(it)
 
-//        logNode(rootNode)
+            //        logNode(it)
+        }
     }
 
     private fun logNode(node: AccessibilityNodeInfo?, prefix: String = "") {
@@ -37,154 +38,6 @@ class WechatAccessibilityService : AccessibilityService() {
             }
             logNode(child, "    ")
         }
-    }
-
-    private fun uploadMessages(node: AccessibilityNodeInfo?) {
-        node ?: return
-        val contentNode = node.findContentNode() ?: return
-        val isFromGroup = contentNode.isFromGroup()
-
-        val messages = mutableListOf<WechatMessage>()
-
-        var messageIndex = 0
-        var time: String? = null
-
-        contentNode.children().forEach {
-            var name: String? = null
-            var nameIndex = 0
-            var content: String? = null
-            var contentIndex = 0
-
-            var redPacketContent: String? = null
-            var mediaType: String? = null
-
-            it.findTimeNode()?.text?.toString()?.let { text ->
-                time = text
-            }
-
-            for (child in it.children()) {
-                if (child.viewIdResourceName in WECHAT_AVATAR_NODE_VIEW_IDS) {
-                    name = child.contentDescription?.replace("头像".toRegex(), "")
-                }
-
-                for ((i, c) in child.children().withIndex()) {
-                    val id = c.viewIdResourceName
-
-                    if (id in WECHAT_AVATAR_NODE_VIEW_IDS && name == null) {
-                        name = c.contentDescription?.replace("头像".toRegex(), "")
-                        nameIndex = i
-                    }
-
-                    if (id in WECHAT_REDPACKET_CONTENT_NODE_VIEW_IDS) {
-                        redPacketContent = c.text?.toString()
-                    }
-
-                    if (id in WECHAT_REDPACKET_NODE_VIEW_IDS && content == null) {
-                        content = "[${c.text?.toString()}]${redPacketContent.orEmpty()}"
-                        contentIndex = i
-                    }
-
-                    if (id in WECHAT_TRANSFER_NODE_VIEW_IDS && content == null) {
-                        content = "[${c.text?.toString()}]"
-                        contentIndex = i
-                    }
-
-                    if (id in WECHAT_SHARE_CONTENT_NODE_VIEW_IDS && content == null) {
-                        content = "[分享]${c.text?.toString()}"
-                        contentIndex = i
-                    }
-
-                    if (id in WECHAT_FILE_CONTENT_NODE_VIEW_IDS && content == null) {
-                        content = "[文件]${c.text?.toString()}"
-                        contentIndex = i
-                    }
-
-                    if (id in WECHAT_VIDEO_CONTENT_NODE_VIEW_IDS && content == null) {
-                        content = "[视频]${c.text?.toString()}"
-                        contentIndex = i
-                    }
-
-                    if (id in WECHAT_IMAGE_CONTENT_NODE_VIEW_IDS && content == null) {
-                        content = "[图片]"
-                        contentIndex = i
-                    }
-
-                    if (id in WECHAT_AUDIO_CONTENT_NODE_VIEW_IDS) {
-                        mediaType = "[语音]"
-                    }
-
-                    if (id in WECHAT_TEXT_CONTENT_NODE_VIEW_IDS && content == null) {
-                        content = "${mediaType.orEmpty()}${c.text?.toString()}"
-                        contentIndex = i
-                    }
-                }
-                if (nameIndex > contentIndex) {
-                    Logger.debug { "自己发送的" }
-                }
-                if (name != null && content != null) {
-                    Logger.debug { "onAccessibilityEvent: $time  $name  $content" }
-                    val message = kotlin.runCatching {
-                        createMessage(
-                            name,
-                            content,
-                            time = time,
-                            from = if (isFromGroup) "群聊" else "个人",
-                            type = 1,
-                            index = messageIndex
-                        )
-                    }.getOrNull()
-
-                    if (message != null) {
-                        messages.add(message)
-                    }
-
-                    messageIndex += 1
-                }
-            }
-        }
-
-        MessagesUploadManager.addMessages(messages)
-    }
-
-    private fun AccessibilityNodeInfo.findContentNode(): AccessibilityNodeInfo? {
-        for (viewId in WECHAT_MESSAGE_LIST_VIEW_IDS) {
-            val node = findAccessibilityNodeInfosByViewId(viewId)
-                ?.firstOrNull()
-            if (node != null) {
-                return node
-            }
-        }
-        return null
-    }
-
-    private fun AccessibilityNodeInfo.findTimeNode(): AccessibilityNodeInfo? {
-        for (viewId in WECHAT_TIME_NODE_VIEW_IDS) {
-            val node = findAccessibilityNodeInfosByViewId(viewId)
-                ?.firstOrNull()
-            if (node != null) {
-                return node
-            }
-        }
-        return null
-    }
-
-    private fun AccessibilityNodeInfo.isFromGroup(): Boolean {
-        for (viewId in WECHAT_GROUND_JUDGE_VIEW_IDS) {
-            val nodes = findAccessibilityNodeInfosByViewId(viewId)
-            if (!nodes.isNullOrEmpty()) {
-                return true
-            }
-        }
-        return false
-    }
-
-    private fun AccessibilityNodeInfo.children(): List<AccessibilityNodeInfo> {
-        val nodes = mutableListOf<AccessibilityNodeInfo>()
-        for (index in 0 until childCount) {
-            val child = getChild(index) ?: continue
-            nodes.add(child)
-        }
-        return nodes
     }
 
     override fun onInterrupt() {
